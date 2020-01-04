f5d38a73f720b129bce2fedc8902b99694afeb23
Maksim Kim <maksim.kim@bellerage.ru>
2020-01-04 14:07:49 +0300

    State introduction

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'product_manager.dart';
 
 main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('MyApp'),
-        ), body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/food.jpeg'),
-          Text('Food paradise'),
-        ],),),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('EasyList'),
+            ),
+            body: ProductManager('Starting Product')));
   }
 }
